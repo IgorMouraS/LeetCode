2729897327d27024f4de3f97e3ac378037caf7f6
Igor Sousa <igormsousa2003@gmail.com>
2023-08-08 11:34:25 -0300

    Concluido

--- 2635.js
+++ 2635.js
@@ -1,20 +1,16 @@
 let map = function (arr, fn) {
-    let array = [];
-    let returnedArray = array.concat(arr);
 
-    for (let i = 0; i < returnedArray.length; i++) {
-        if(fn = plusone(n)){
-            returnedArray[i] = returnedArray[i] + 1;
-        }else if(plusI(n, i)){
-            returnedArray[i] = returnedArray[i] + i;
-        }else{
-            returnedArray[i] = 42;
-        }
+    let returnedArray = arr;
+    
+    for (let i = 0; i < arr.length; i++) {
+
+        returnedArray[i] = fn(arr[i], i);  
+
     };
+    
     return returnedArray;
-
 };
 
 
-let result = map([2, 1, 3], 1);
+let result = map([1,2,3], function constant() { return 42; });
 console.log(result);
