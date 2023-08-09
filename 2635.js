0f1ea8028d642e5b0b3e2183733a77fceba69d02
Igor Sousa <igormsousa2003@gmail.com>
2023-08-09 10:19:39 -0300

    Concluito refatorado

--- 2635.js
+++ 2635.js
@@ -1,16 +1,14 @@
 let map = function (arr, fn) {
-
-    let returnedArray = arr;
+    let returnedArray = [];
     
-    for (let i = 0; i < arr.length; i++) {
-
-        returnedArray[i] = fn(arr[i], i);  
-
+    for (const i in arr) {
+        returnedArray.push(fn(arr[i], Number(i)));  
     };
     
     return returnedArray;
+    //return arr.map(fn);
 };
 
 
-let result = map([1,2,3], function constant() { return 42; });
+let result = map([1,2,3], function plusI(n, i) { return n + i; });
 console.log(result);
