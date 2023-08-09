8879782d0b0cf6bd3f2c5b44eae6574354cd4d1b
Igor Sousa <igormsousa2003@gmail.com>
2023-08-09 10:31:06 -0300

    Concluido refatorado

--- 2634.js
+++ 2634.js
@@ -1,15 +1,15 @@
 let filter = function(arr, fn) {
     let filteredArr = [];
 
-    for(let i = 0; i < arr.length; i++){
-        let result = fn(arr[i], i);
+    for(const i in arr){
 
-        if(result){
+        if(fn(arr[i], Number(i))){
             filteredArr.push(arr[i]);
         }
     }
     
     return filteredArr;
+    //return arr.filter((a, i)=>fn(a, i));
 };
 
 let teste = filter([-2,-1,0,1,2], function plusOne(n) { return n + 1 });
