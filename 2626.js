0790a814b6f4db17557790827cf2e1ae127515e8
Igor Moura Sousa <igormsousa2003@gmail.com>
2023-08-10 21:23:16 -0300

    Concluido refatorado

--- 2626.js
+++ 2626.js
@@ -1,17 +1,12 @@
-var reduce = function(nums, fn, init) {
-    let reduced = [];
-
-    if(nums.length === 0){
-        return reduced = init;
-    }
+let reduce = function(nums, fn, init) {
     
     for(let i = 0;  i < nums.length; i++){
         init = fn(init, nums[i]);
     }
 
-    
-    return reduced = init;
+    return init;
+    //return nums.reduce((val, num) => fn(val,num), init);
 };
 
-let result = reduce([], function sum(accum, curr) { return accum + curr; }, 25);
+let result = reduce([1, 2, 3, 4], function sum(accum, curr) { return 0; }, 0);
 console.log(result);
